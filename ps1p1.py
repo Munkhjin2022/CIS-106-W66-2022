c019a51216f3352e1221a44bb469c814ec43010d
Munkhjin2022 <114359135+Munkhjin2022@users.noreply.github.com>
2022-09-25 23:06:30 -0500

    Create ps1p1.py

---
+++ ps1p1.py
@@ -0,0 +1,13 @@
+#comment input 
+Number1 = float(input("Enter a number "))
+Number2 = float(input("Enter another number "))
+
+#process phase 
+Sum = Number1 + Number2 
+Difference = Number1 - Number2
+Product = Number1 * Number2
+
+#output
+print ("The sum is ", Sum) 
+print ("The difference is ", Difference)
+print ("The product is ", Product) 
