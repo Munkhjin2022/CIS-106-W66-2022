c286d723fc2a0aa7808a42e9488d98b64f070273
Munkhjin2022 <114359135+Munkhjin2022@users.noreply.github.com>
2022-09-25 23:09:06 -0500

    Create ps1p4.py

---
+++ ps1p4.py
@@ -0,0 +1,10 @@
+#comment input
+A = float(input("Please enter your amount of meal"))
+
+#process phase
+t = (A/100) * 15
+Total = A + t
+
+#output
+print ("The tip to be 15% is", t)
+print ("The total is", Total)
