a7e588202edc226148807cfb6e5bf5966f2ceb55
Munkhjin2022 <114359135+Munkhjin2022@users.noreply.github.com>
2022-09-25 23:08:35 -0500

    Create ps1p3.py

---
+++ ps1p3.py
@@ -0,0 +1,9 @@
+#comment input
+gl = float(input("Please enter the gallons used for trip"))
+ml = float(input("Then enter the mile you traveled"))
+
+#process phase
+p = ml/gl
+
+#output
+print("Your mile per gallon is ", p)
