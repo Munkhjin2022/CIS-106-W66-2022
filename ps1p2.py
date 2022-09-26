72f145dbc48a4aa00e36f23b9679262329612f9c
Munkhjin2022 <114359135+Munkhjin2022@users.noreply.github.com>
2022-09-25 23:07:43 -0500

    Create ps1p2.py

---
+++ ps1p2.py
@@ -0,0 +1,11 @@
+#comment input
+Name = str(input("Please enter the last name"))
+Score1 = float(input("Then enter the Score1"))
+Score2 = float(input("Enter the Score2"))
+
+#process phase
+A = (Score1 + Score2)/2
+
+#output
+print ("Hello ", Name)
+print ("Your average score is ", A)
